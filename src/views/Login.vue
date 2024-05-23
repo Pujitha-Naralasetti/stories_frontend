f556ad52e71ffecd8861bbbbad37525a8bc6cfd8
Anusha <anushaveeragoni@gmail.com>
2024-05-23 12:11:18 -0500

    login-and-create-account-changes

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,13 @@
 <script setup>
 import { onMounted } from "vue";
-import { ref, toRaw } from "vue";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 import UserServices from "../services/UserServices.js";
 
 const router = useRouter();
 const isCreateAccount = ref(false);
+const loginType = ref("user");
+const emailInValid = ref(false);
 const snackbar = ref({
   value: false,
   color: "",
@@ -25,136 +27,133 @@ onMounted(async () => {
   // }
 });
 
-function navigateToRecipes() {
-  router.push({ name: "recipes" });
-}
-
 async function createAccount() {
-  await UserServices.addUser(user.value)
-    .then(() => {
-      snackbar.value.value = true;
-      snackbar.value.color = "green";
-      snackbar.value.text = "Account created successfully!";
-      router.push({ name: "login" });
-    })
-    .catch((error) => {
-      console.log(error);
-      snackbar.value.value = true;
-      snackbar.value.color = "error";
-      snackbar.value.text = error.response.data.message;
-    });
+  // await UserServices.addUser(user.value)
+  //   .then(() => {
+  //     emailInValid.value = false;
+  //     snackbar.value.value = true;
+  //     snackbar.value.color = "green";
+  //     snackbar.value.text = "Account created successfully!";
+  //     isCreateAccount.value = false;
+  //     user.value = {
+  //       firstName: "",
+  //       lastName: "",
+  //       email: "",
+  //       password: "",
+  //     };
+  //     router.push({ name: "login" });
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //     snackbar.value.value = true;
+  //     snackbar.value.color = "error";
+  //     snackbar.value.text = error.response.data.message;
+  //   });
 }
 
 async function login() {
-  await UserServices.loginUser(user)
-    .then((data) => {
-      window.localStorage.setItem("user", JSON.stringify(data.data));
-      snackbar.value.value = true;
-      snackbar.value.color = "green";
-      snackbar.value.text = "Login successful!";
-      router.push({ name: "recipes" });
-    })
-    .catch((error) => {
-      console.log(error);
-      snackbar.value.value = true;
-      snackbar.value.color = "error";
-      snackbar.value.text = error.response.data.message;
-    });
+  // await UserServices.loginUser(user)
+  //   .then((data) => {
+  //     window.localStorage.setItem("user", JSON.stringify(data.data));
+  //     snackbar.value.value = true;
+  //     snackbar.value.color = "green";
+  //     snackbar.value.text = "Login successful!";
+  //     user.value = {
+  //       firstName: "",
+  //       lastName: "",
+  //       email: "",
+  //       password: "",
+  //     };
+  //     router.push({ name: "stories" });
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //     snackbar.value.value = true;
+  //     snackbar.value.color = "error";
+  //     snackbar.value.text = error.response.data.message;
+  //   });
 }
 
 function openCreateAccount() {
+  emailInValid.value = false;
   isCreateAccount.value = true;
+  user.value = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  };
 }
 
 function closeCreateAccount() {
+  emailInValid.value = false;
   isCreateAccount.value = false;
+  user.value = {
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+  };
 }
 
 function closeSnackBar() {
   snackbar.value.value = false;
 }
+
+function onEmailChange() {
+  if (user.value.email) {
+    if (user.value.email.match(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i)) {
+      emailInValid.value = false;
+    } else {
+      emailInValid.value = true;
+    }
+  } else {
+    emailInValid.value = false;
+  }
+}
 </script>
 
 <template>
   <v-container>
-    <div id="body">
-      <v-card class="rounded-lg elevation-5">
-        <v-card-title class="headline mb-2">Login </v-card-title>
+    <div id="wrapper">
+      <v-card id="body" color="#232323" class="rounded-lg" max-width="600" dark>
+        <v-card-title class="headline mb-2 text-center">Login</v-card-title>
         <v-card-text>
-          <v-text-field
-            v-model="user.email"
-            label="Email"
-            required
-          ></v-text-field>
+          <v-text-field v-model="user.email" label="Email*" :hint="emailInValid && 'Please enter valid email'"
+            @input="onEmailChange" required></v-text-field>
 
-          <v-text-field
-            v-model="user.password"
-            label="Password"
-            required
-          ></v-text-field>
+          <v-text-field v-model="user.password" label="Password*" type="password" required></v-text-field>
         </v-card-text>
         <v-card-actions>
-          <v-btn variant="flat" color="secondary" @click="openCreateAccount()"
-            >Create Account</v-btn
-          >
+          <v-btn v-if="loginType == 'user'" variant="outlined" @click="openCreateAccount()">Create
+            Account</v-btn>
           <v-spacer></v-spacer>
-
-          <v-btn variant="flat" color="primary" @click="login()">Login</v-btn>
+          <v-btn variant="outlined" :disabled="emailInValid || !user.email || !user.password"
+            @click="login()">Login</v-btn>
         </v-card-actions>
       </v-card>
 
-      <v-card class="rounded-lg elevation-5 my-8">
-        <v-card-title class="text-center headline">
-          <v-btn
-            class="ml-2"
-            variant="flat"
-            color="secondary"
-            @click="navigateToRecipes()"
-          >
-            View Published Recipes
-          </v-btn>
-        </v-card-title>
-      </v-card>
-
-      <v-dialog persistent v-model="isCreateAccount" width="800">
-        <v-card class="rounded-lg elevation-5">
-          <v-card-title class="headline mb-2">Create Account </v-card-title>
+      <v-dialog persistent v-model="isCreateAccount" width="700">
+        <v-card color="#232323" class="rounded-lg elevation-5">
+          <v-card-title class="headline mb-2">Create User Account</v-card-title>
           <v-card-text>
-            <v-text-field
-              v-model="user.firstName"
-              label="First Name"
-              required
-            ></v-text-field>
+            <v-text-field v-model="user.firstName" label="First Name*" required></v-text-field>
 
-            <v-text-field
-              v-model="user.lastName"
-              label="Last Name"
-              required
-            ></v-text-field>
+            <v-text-field v-model="user.lastName" label="Last Name*" required></v-text-field>
 
-            <v-text-field
-              v-model="user.email"
-              label="Email"
-              required
-            ></v-text-field>
+            <v-text-field v-model="user.email" label="Email*" :hint="emailInValid && 'Please enter valid email'"
+              @input="onEmailChange" required></v-text-field>
 
-            <v-text-field
-              v-model="user.password"
-              label="Password"
-              required
-            ></v-text-field>
+            <v-text-field v-model="user.password" label="Password*" required type="password"></v-text-field>
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
-            <v-btn
-              variant="flat"
-              color="secondary"
-              @click="closeCreateAccount()"
-              >Close</v-btn
-            >
-            <v-btn variant="flat" color="primary" @click="createAccount()"
-              >Create Account</v-btn
-            >
+            <v-btn @click="closeCreateAccount()">Close</v-btn>
+            <v-btn variant="outlined" :disabled="emailInValid ||
+              !user.email ||
+              !user.password ||
+              !user.firstName ||
+              !user.lastName" @click="createAccount()">Create Account</v-btn>
           </v-card-actions>
         </v-card>
       </v-dialog>
@@ -163,11 +162,7 @@ function closeSnackBar() {
         {{ snackbar.text }}
 
         <template v-slot:actions>
-          <v-btn
-            :color="snackbar.color"
-            variant="text"
-            @click="closeSnackBar()"
-          >
+          <v-btn :color="snackbar.color" variant="text" @click="closeSnackBar()">
             Close
           </v-btn>
         </template>
@@ -175,3 +170,17 @@ function closeSnackBar() {
     </div>
   </v-container>
 </template>
+
+<style scoped>
+#wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 85vh;
+}
+
+#body {
+  width: 100%;
+  max-width: 600px;
+}
+</style>
